8c0d0a315bb3bbe9cd56d4487ceff58b81d0f43e
Sébastien BRISARD <sebastien.brisard@univ-eiffel.fr>
2020-04-10 07:05:50 +0200

    CartesianGrid: ctor has default value for L

--- src/bri17.cpp
+++ src/bri17.cpp
@@ -27,7 +27,9 @@ class CartesianGrid {
   double L[DIM];
   size_t N[DIM];
 
-  CartesianGrid(size_t N[], double L[])
+  // Default value for L may have inconsistent dimension, but only the
+  // DIM first elements will be considered.
+  CartesianGrid(size_t N[], double L[] = {1., 1., 1.})
       : num_nodes_per_cell{1 << DIM}, num_cells{product(DIM, N)} {
     static_assert((DIM == 2) || (DIM == 3));
     for (size_t i = 0; i < DIM; i++) {
